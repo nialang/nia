8311da7336e89f23636644d5bd73190b327b881e
softfault <givenyoung46@gmail.com>
2026-09-16 08:07:16 +0800

    style(build): format runner cache imports

--- crates/nia-build/src/runner_cache.rs
+++ crates/nia-build/src/runner_cache.rs
@@ -5,9 +5,7 @@ use std::fs;
 use std::io;
 use std::path::PathBuf;
 
-use nia_compat::{
-    COMPILER_VERSION, RELEASE_COMPATIBILITY, toolchain::BUILD_PROTOCOL,
-};
+use nia_compat::{COMPILER_VERSION, RELEASE_COMPATIBILITY, toolchain::BUILD_PROTOCOL};
 use nia_query::FingerprintDomain;
 
 use crate::{BuildError, BuildInvocation, BuildRunnerSource, OptimizationMode};
